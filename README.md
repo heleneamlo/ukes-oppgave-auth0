73c19312f77b3d9bc0e712588f46a90975649796
Henning Amlo <72279725+henningamlo@users.noreply.github.com>
2022-04-27 23:41:10 +0200

    fixed so that auth0 works

--- README.md
+++
@@ -1,2 +0,0 @@
-# ukes-oppgave-auth0
-denne oppgaven er laget på 3 dager og er om autentisering med auth0 autentiseringsrammeverk
--- public/js/app.js
+++ public/js/app.js
@@ -1,59 +1,128 @@
+// The Auth0 client, initialized in configureClient()
 let auth0 = null;
 
-const fetchAuthConfig = () => fetch("/auth_config.json");
-//oauth0 config
-const configureClient = async () => {
-    const response = await fetchAuthConfig();
-    const config = await response.json();
-  
-    auth0 = await createAuth0Client({
-      domain: config.domain,
-      client_id: config.clientId
-    });
-};
-//funksjon som skal sjekke om login for brukeren er cacha på side innlasting
-window.onload = async () => {
-  
-    updateUI();
-  
-    const isAuthenticated = await auth0.isAuthenticated();
-  
-    if (isAuthenticated) {
-      document.getElementById("gated-content").classList.remove("hidden");
-      return;
-    }
-  
-    const query = window.location.search;
-    if (query.includes("code=") && query.includes("state=")) {
-  
-      await auth0.handleRedirectCallback();
-      
-      updateUI();
-  
-      window.history.replaceState({}, document.title, "/");
-    }
-};
-//oppdater user interface
-const updateUI = async () => { 
-    const isAuthenticated = await auth0.isAuthenticated();
-  
+/**
+ * Starts the authentication flow
+ */
+const login = async (targetUrl) => {
+  try {
+    console.log("Logging in", targetUrl);
 
-    //hvis autentisert fjern klasser gated-content (MIDLERTIDIG)
-    if (isAuthenticated) {
-      document.getElementById("gated-content").classList.remove("hidden");
-    } else {
-      document.getElementById("gated-content").classList.add("hidden");
-    }
-  };
-//logg inn funksjon
-const login = async () => {
-    await auth0.loginWithRedirect({
+    const options = {
       redirect_uri: window.location.origin
-    });
+    };
+
+    if (targetUrl) {
+      options.appState = { targetUrl };
+    }
+
+    await auth0.loginWithRedirect(options);
+  } catch (err) {
+    console.log("Log in failed", err);
+  }
 };
-//logg ut funksjon
+
+/**
+ * Executes the logout flow
+ */
 const logout = () => {
+  try {
+    console.log("Logging out");
     auth0.logout({
       returnTo: window.location.origin
     });
-  };
+  } catch (err) {
+    console.log("Log out failed", err);
+  }
+};
+
+/**
+ * Retrieves the auth configuration from the server
+ */
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+/**
+ * Initializes the Auth0 client
+ */
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+
+  auth0 = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId
+  });
+};
+
+/**
+ * Checks to see if the user is authenticated. If so, `fn` is executed. Otherwise, the user
+ * is prompted to log in
+ * @param {*} fn The function to execute if the user is logged in
+ */
+const requireAuth = async (fn, targetUrl) => {
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  if (isAuthenticated) {
+    return fn();
+  }
+
+  return login(targetUrl);
+};
+
+// Will run when page finishes loading
+window.onload = async () => {
+  await configureClient();
+
+  // If unable to parse the history hash, default to the root URL
+  if (!showContentFromUrl(window.location.pathname)) {
+    showContentFromUrl("/");
+    window.history.replaceState({ url: "/" }, {}, "/");
+  }
+
+  const bodyElement = document.getElementsByTagName("body")[0];
+
+  // Listen out for clicks on any hyperlink that navigates to a #/ URL
+  bodyElement.addEventListener("click", (e) => {
+    if (isRouteLink(e.target)) {
+      const url = e.target.getAttribute("href");
+
+      if (showContentFromUrl(url)) {
+        e.preventDefault();
+        window.history.pushState({ url }, {}, url);
+      }
+    }
+  });
+
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  if (isAuthenticated) {
+    console.log("> User is authenticated");
+    window.history.replaceState({}, document.title, window.location.pathname);
+    updateUI();
+    return;
+  }
+
+  console.log("> User not authenticated");
+
+  const query = window.location.search;
+  const shouldParseResult = query.includes("code=") && query.includes("state=");
+
+  if (shouldParseResult) {
+    console.log("> Parsing redirect");
+    try {
+      const result = await auth0.handleRedirectCallback();
+
+      if (result.appState && result.appState.targetUrl) {
+        showContentFromUrl(result.appState.targetUrl);
+      }
+
+      console.log("Logged in!");
+    } catch (err) {
+      console.log("Error parsing redirect:", err);
+    }
+
+    window.history.replaceState({}, document.title, "/");
+  }
+
+  updateUI();
+};
